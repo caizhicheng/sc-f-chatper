7c8d43f11eb6a422feb6e040478d8cb80c7156e9
caizhicheng <412388726@qq.com>
2018-08-05 21:02:46 +0800

    Initial commit

---
+++ src/main/java/com/forezp/scfchatper/ScFChatperApplication.java
@@ -0,0 +1,12 @@
+package com.forezp.scfchatper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScFChatperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScFChatperApplication.class, args);
+    }
+}
